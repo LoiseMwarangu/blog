3707510ba85af2a7945045fee4a7a9e5581cdc18
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-19 12:43:16 +0300

    Deployment

--- app/auth/views.py
+++ app/auth/views.py
@@ -36,7 +36,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message("Welcome to Pitch App","email/welcome_user",user.email,user=user)
+       # mail_message("Welcome to Pitch App","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
@@ -49,7 +49,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+        #mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
