a405824dc6292a781c0a6a9ee4b9d9a546357461
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-19 12:59:21 +0300

    Changed config

---
+++ start.sh
@@ -0,0 +1,9 @@
+export MAIL_USERNAME='loisemuwarangu@gmail.com'
+export MAIL_PASSWORD='loise@0000009954'
+export SECRET_KEY='1234'
+export FLASK_APP='app.py'
+export FLASK_DEBUG=1
+
+python3.6 manage.py server
+
+
